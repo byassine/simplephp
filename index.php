847b68a453cd6431d260b7337d3a092754d1dc66
byassine <bouderaa.yassine@gmail.com>
2024-05-26 00:35:18 +0100

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,6 @@
  </head>
  <body>
  <?php echo '<p>Hello World</p>'; ?> 
-  <?php echo '<p>Yassine Test</p>'; ?> 
-  <?php echo '<p>Test</p>'; ?>
+  <?php echo '<p>Ajout d'un code analyset</p>'; ?> 
  </body>
 </html>
